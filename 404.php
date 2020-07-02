07d02e74ece632acf5133c02c9e11321082d883c
tungocvan <tungocvan@gmail.com>
2020-07-02 09:33:16 +0700

    nav-002

--- 404.php
+++ 404.php
@@ -4,57 +4,33 @@
  *
  * @link https://codex.wordpress.org/Creating_an_Error_404_Page
  *
- * @package tungocvan
+ * @package azshopweb
  */
 
-get_header();
+$uri = $_SERVER['REQUEST_URI'];
+$array = explode('/', $uri);
+echo '<pre>';print_r($array);echo '</pre>';
+if($array[2] == "product") {
+	if($array[3] !="") {	
+		if($array[4] !="") {
+			echo "-  slug: ".$array[4];
+			$slug = $array[4];
+		}else{
+			echo "product slug: ".$array[3];
+			$cateSLug = $array[3];
+		}
+	}
+}else if($array[2] == "post") {
+	if($array[3] !="") {		
+		if($array[4] !="") {
+			echo "- slug: ".$array[4];
+			$slug = $array[4];
+		}else{
+			echo "post slug: ".$array[3];
+			$cateSLug = $array[3];
+		}
+	}
+}else {
+	echo "path not found";
+}
 ?>
-
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'tungocvan' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'tungocvan' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'tungocvan' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$tungocvan_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'tungocvan' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$tungocvan_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
-
-<?php
-get_footer();
